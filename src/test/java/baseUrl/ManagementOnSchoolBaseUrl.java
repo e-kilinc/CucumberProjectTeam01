ae2ed90e70d2ccac496486d2ce7e25e4966d6bbc
Yasemin Eric <yasemineric6@gmail.com>
2023-09-12 13:42:02 +0200

    12.09

--- src/test/java/baseUrl/ManagementOnSchoolBaseUrl.java
+++ src/test/java/baseUrl/ManagementOnSchoolBaseUrl.java
@@ -1,4 +1,20 @@
 package baseUrl;
 
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+
+import static utilities.Authentication.generateToken;
+
 public class ManagementOnSchoolBaseUrl {
+
+        public static RequestSpecification spec;
+        public static void setup() {
+            spec = new RequestSpecBuilder()
+                    .setBaseUri("https://managementonschools.com/app")
+                    .addHeader("Authorization", generateToken())
+                    .setContentType(ContentType.JSON)
+                    .build();
+        }
+
 }
